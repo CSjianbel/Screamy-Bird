663f60820315819add031be6e5c9f209b784d92e
OjouNii <jibelarmino@gbox.adnu.edu.ph>
2023-10-06 22:15:47 +0800

    feat: using cat sprites instead of bird

--- src/game/utils/sprites.py
+++ src/game/utils/sprites.py
@@ -8,8 +8,8 @@ SPRITES_DIR = os.path.join(ASSETS_DIR, 'sprites')
 class Sprites:
     def __init__(self):
         self.bird = tuple([
-            pygame.image.load(os.path.join(SPRITES_DIR, f'bird{i}.png'))
-            for i in range(1, 4)
+            pygame.image.load(os.path.join(SPRITES_DIR, f'cat{i}.png'))
+            for i in range(1, 7)
         ])
         self.background = pygame.image.load(os.path.join(SPRITES_DIR, 'background.png'))
         self.pipe = pygame.image.load(os.path.join(SPRITES_DIR, 'pipe.png'))
